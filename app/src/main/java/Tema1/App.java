08a07fbe72c662d382b7a0ae0cc6e93e531fa05f
Maxim Durnea <70955732+max-durnea@users.noreply.github.com>
2024-11-15 13:22:26 +0200

    Initial commit

---
+++ app/src/main/java/Tema1/App.java
@@ -0,0 +1,24 @@
+package Tema1;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.text.*;
+
+public class App {
+    private Scanner scanner;
+
+    public App(InputStream input) {
+        this.scanner = new Scanner(input);
+    }
+
+    public void run() {
+        // Implementați aici cerințele din enunț
+        // Pentru citirea datelor de la tastatura se folosește câmpul scanner.
+    }
+
+    public static void main(String[] args) {
+        App app = new App(System.in);
+        app.run();
+    }
+}
